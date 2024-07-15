9999c42fb9bffee61b18e1fcc33cfc4f090923bd
Arpit Rautela <117685589+ArpitRautela@users.noreply.github.com>
2024-07-15 23:17:41 +0530

    Create FindAllDuplicatesInAnArray.cpp

---
+++ DAY1/FindAllDuplicatesInAnArray.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> ans;
+        std::sort(nums.begin(),nums.end());
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]==nums[i-1]) ans.push_back(nums[i]);
+        }
+        return ans;   
+    }
+};
